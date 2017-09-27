a73bb66715eace3a14e497eb752874b125660636
Diego226 <d.duque@fisfun.uned.es>
2017-09-27 13:05:17 +0200

    Deleted for loop in crossCorrelation

--- crossCorrelation.py
+++ crossCorrelation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Nov 14 2016
+Modified on Wed Sep 27 2017
 
 Autor: DiegoDZ
 
@@ -21,27 +22,20 @@ def crossCorrelation(arg1,arg2):
 
     #Define params
     nSteps = len(A)
-    nNodes = len(A[0])
 
     #Cross-correlation works as follows:
     #1. Take FFT of each column of both input matrix
     #2. Multiply one resulting transform by the complex conjugate of the other
     #3. Calculate the inverse transform of the product
 
-    a = np.zeros((nSteps, nNodes),dtype=complex)
-    b = np.zeros((nSteps, nNodes),dtype=complex)
-    for i in range(0,nNodes,1):
-        a[:,i] = np.fft.fft(A[:,i] - np.mean(A[:,i]))
-        b[:,i] = np.conjugate(np.fft.fft(B[:,i]- np.mean(B[:,i])))
+    a = np.fft.fft(A - np.mean(A,axis=0),axis=0)
+    b = np.conjugate(np.fft.fft(B - np.mean(B,axis=0),axis=0))
     #Multiply all columns of matrix a by all columns of matrix b.
     C = (a[...,None]*b[:,None]).reshape(a.shape[0],-1)
     #Calculate the inverse
-    CAB = np.zeros((nSteps, nNodes*nNodes))
-    for j in range (0, nNodes*nNodes, 1):
-        CAB[:,j] = np.fft.ifft(C[:,j])
-    CAB /= nSteps
+    CAB = np.fft.ifft(C,axis=0).real / nSteps
 
-    return CAB[0 : nSteps/2, :]
+    return CAB[0:nSteps/2,:]
 
 CAB = crossCorrelation(sys.argv[1],sys.argv[2])
 
